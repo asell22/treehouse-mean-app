8e660bbbea0c25c78b6e8ba656751b512757a194
asell22 <alexanderjsell@gmail.com>
2016-03-19 21:56:28 -0700

    mock data and express router

--- src/app.js
+++ src/app.js
@@ -2,16 +2,11 @@
 
 var express = require('express');
 var app = express();
+var router = require('./api');
 
 app.use('/', express.static('public'));
 
-var router = express.Router();
-
 app.use('/api', router);
-
-router.get('/todos', function(req, res) {
-  res.json({todos: []})
-})
 
 app.listen(3000, function(req, res) {
   console.log("The server is running on Port 3000!");
